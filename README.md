9d4acdc5e5a3be7346e3869e2e144042dea1b377
Norbert Orzechowicz <norbert@orzechowicz.pl>
2020-11-18 21:22:03 +0100

    Increased tests coverage

---
+++ README.md
@@ -0,0 +1,20 @@
+# Aeon
+
+[![Minimum PHP Version](https://img.shields.io/badge/php-%3E%3D%207.4-8892BF.svg)](https://php.net/)
+[![Latest Stable Version](https://poser.pugx.org/aeon-php/rate-limiter/v)](//packagist.org/packages/aeon-php/rate-limiter)
+[![Latest Unstable Version](https://poser.pugx.org/aeon-php/rate-limiter/v/unstable)](//packagist.org/packages/aeon-php/rate-limiter)
+[![License](https://poser.pugx.org/aeon-php/rate-limiter/license)](//packagist.org/packages/aeon-php/rate-limiter)
+![Tests](https://github.com/aeon-php/rate-limiter/workflows/Tests/badge.svg?branch=1.x)
+[![Mutation testing badge](https://img.shields.io/endpoint?style=flat&url=https%3A%2F%2Fbadge-api.stryker-mutator.io%2Fgithub.com%2Faeon-php%2Frate-limiter%2F1.x)](https://dashboard.stryker-mutator.io/reports/github.com/aeon-php/rate-limiter/1.x)
+
+Time Management Framework for PHP
+
+> The word aeon /ˈiːɒn/, also spelled eon (in American English), originally meant "life", "vital force" or "being", 
+> "generation" or "a period of time", though it tended to be translated as "age" in the sense of "ages", "forever", 
+> "timeless" or "for eternity".
+
+[Source: Wikipedia](https://en.wikipedia.org/wiki/Aeon) 
+
+Aeon is a set of libraries that makes easier to work with PHP Date & Time in elegant Object Oriented way.
+
+Please read [Official Documentation](https://aeon-php.org/docs/rate-limiter/).
---
+++ tests/Aeon/RateLimiter/Tests/Unit/RateLimiterTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aeon\RateLimiter\Tests\Unit;
+
+use Aeon\Calendar\System\Process;
+use Aeon\Calendar\TimeUnit;
+use Aeon\RateLimiter\Algorithm;
+use Aeon\RateLimiter\Exception\RateLimitException;
+use Aeon\RateLimiter\RateLimiter;
+use Aeon\RateLimiter\Storage;
+use PHPUnit\Framework\TestCase;
+
+final class RateLimiterTest extends TestCase
+{
+    public function test_hit_method_throwing_exception() : void
+    {
+        $algorithm = $this->createStub(Algorithm::class);
+        $algorithm->method('hit')->willThrowException(new RateLimitException('id', TimeUnit::seconds(10)));
+
+        $rateLimiter = new RateLimiter(
+            $algorithm,
+            $this->createMock(Storage::class)
+        );
+
+        $this->expectExceptionMessage(RateLimitException::class);
+        $this->expectExceptionMessage('Execution "id" was limited for the next 10.000000 seconds');
+
+        $rateLimiter->hit('id');
+    }
+
+    public function test_estimate_method_throwing_exception() : void
+    {
+        $algorithm = $this->createStub(Algorithm::class);
+        $algorithm->method('nextHit')->willReturn(TimeUnit::second());
+
+        $rateLimiter = new RateLimiter(
+            $algorithm,
+            $this->createMock(Storage::class)
+        );
+
+        $this->assertSame(1, $rateLimiter->estimate('id')->inSeconds());
+    }
+
+    public function test_throttle_without_waiting() : void
+    {
+        $algorithm = $this->createStub(Algorithm::class);
+
+        $rateLimiter = new RateLimiter(
+            $algorithm,
+            $this->createMock(Storage::class)
+        );
+
+        $process = $this->createMock(Process::class);
+        $process->expects($this->never())->method('sleep');
+
+        $rateLimiter->throttle('id', $process);
+    }
+
+    public function test_throttle_and_wait() : void
+    {
+        $algorithm = $this->createMock(Algorithm::class);
+        $algorithm->expects($this->exactly(2))
+            ->method('hit')
+            ->willReturnOnConsecutiveCalls(
+                $this->throwException(new RateLimitException('id', $sleepTime = TimeUnit::seconds(10))),
+                null
+            );
+
+        $rateLimiter = new RateLimiter(
+            $algorithm,
+            $this->createMock(Storage::class)
+        );
+
+        $process = $this->createMock(Process::class);
+        $process->expects($this->once())->method('sleep')->with($sleepTime);
+
+        $rateLimiter->throttle('id', $process);
+    }
+}
